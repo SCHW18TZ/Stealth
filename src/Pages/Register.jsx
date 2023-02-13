720a5ba8534e942174d3657418bb9b7bd5e57a0e
SCHWITZ <63994977+SCHW18TZ@users.noreply.github.com>
2023-02-13 11:46:30 +0530

    Update Register.jsx

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom"
 import { useNavigate } from "react-router-dom";
 import { Toaster, toast } from "react-hot-toast";
 import GoogleLogin from "../Components/GoogleLogin";
