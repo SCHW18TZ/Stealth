c564ac6e253eaa6b368922b1ed19142af6bf96ca
Schwitz <schwitz6969@gmail.com>
2023-02-28 12:18:12 +0530

    Added loader on navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,9 +6,29 @@ import { Link } from "react-router-dom";
 import { signOut } from "firebase/auth";
 import { toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import SyncLoader from "react-spinners/SyncLoader";
+
 const Navbar = () => {
+  const [LoadingUser, setLoadingUser] = useState(true);
   const Navigate = useNavigate();
-  const [user] = useAuthState(auth);
+  // const FetchUser = async () => {
+  //   setLoadingUser(true);
+  //   const [user, loading, error] = useAuthState(auth);
+  //   setLoadingUser(false);
+  // };
+
+  async function FetchUser() {
+    // after 2 seconds make it false
+    setTimeout(() => {
+      setLoadingUser(false);
+    }, 1000);
+  }
+
+  const [user, loading, error] = useAuthState(auth);
+
+  FetchUser();
+
   const LogUserOut = () => {
     signOut(auth);
     toast.success("Logged Out Successfully");
@@ -17,36 +37,46 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
-      <a href="/" className="logo">
-        <img src={Logo} alt="logo" />
-      </a>
-      <ul className="list-items">
-        {!user ? (
-          <div className="unauthorized-list">
-            <Link to="/login" className="links">
-              <button className="Tilted-button">Sign In</button>
-            </Link>
-            <Link to="/register" className="links">
-              <button className="Tilted-button invert">Sign Up</button>
-            </Link>
-          </div>
-        ) : (
-          <div className="authorized-list">
-            <Link to="/create" className="links">
-              <button>Create Post</button>
-            </Link>
-            <Link to="/inbox" className="links">
-              <button>Inbox</button>
-            </Link>
-            <Link to={`/user/${user.uid}`} className="links">
-              <button>My Account</button>
-            </Link>
-            <button className="nav-log-out" onClick={LogUserOut}>
-              Log Out
-            </button>
-          </div>
-        )}
-      </ul>
+      {LoadingUser ? (
+        <div className="loading-navbar">
+          <SyncLoader color="#fff" size={10} />
+        </div>
+      ) : (
+        <>
+          <a href="/" className="logo">
+            <img src={Logo} alt="logo" />
+          </a>
+          <ul className="list-items">
+            {!user ? (
+              <div className="unauthorized-list">
+                <Link to="/login" className="links">
+                  <button className="Tilted-button">Sign In</button>
+                </Link>
+                <Link to="/register" className="links">
+                  <button className="Tilted-button invert">Sign Up</button>
+                </Link>
+              </div>
+            ) : (
+              <>
+                <div className="authorized-list">
+                  <Link to="/create" className="links">
+                    <button>Create Post</button>
+                  </Link>
+                  <Link to="/inbox" className="links">
+                    <button>Inbox</button>
+                  </Link>
+                  <Link to={`/user/${user.uid}`} className="links">
+                    <button>My Account</button>
+                  </Link>
+                  <button className="nav-log-out" onClick={LogUserOut}>
+                    Log Out
+                  </button>
+                </div>
+              </>
+            )}
+          </ul>
+        </>
+      )}
     </nav>
   );
 };
